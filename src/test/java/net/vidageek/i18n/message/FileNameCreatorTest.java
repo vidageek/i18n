7455e155ef84fec7389f84eb901e1f5bd979569a
Jonas Abreu <jonas@vidageek.net>
2009-11-22 01:48:30 -0200

    adding tests got FileNameCreator

---
+++ src/test/java/net/vidageek/i18n/message/FileNameCreatorTest.java
@@ -0,0 +1,25 @@
+package net.vidageek.i18n.message;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * @author jonasabreu
+ * 
+ */
+final public class FileNameCreatorTest {
+
+    @Test
+    public void testThatCreatesFallbackNameForNotSetLanguage() {
+        FileNameCreator creator = new FileNameCreator("baseName");
+        Assert.assertEquals("/baseName.properties", creator.createFileNameFor(new NotSetLanguage()));
+    }
+
+    @Test
+    public void testThatCreatesNameForLanguage() {
+        FileNameCreator creator = new FileNameCreator("baseName");
+        Assert.assertEquals("/baseName_pt_BR.properties", creator.createFileNameFor(new DefaultLanguage("pt_BR")));
+    }
+
+}
--- src/test/java/net/vidageek/i18n/message/MessageProducerTest.java
+++ src/test/java/net/vidageek/i18n/message/MessageProducerTest.java
@@ -19,14 +19,21 @@ final public class MessageProducerTest {
     @Test
     public void testThatUsesFallbackIfCouldNotFindLanguageBundle() {
         MessageProducer producer = new MessageProducer(new FileNameCreator("messages"));
-        String message = producer.getMessage("test", new DefaultLanguage("some_code"));
+        String message = producer.getMessage("test", new DefaultLanguage("country_code"));
         Assert.assertEquals("this is a test", message);
     }
 
     @Test
     public void testThatShowsKeyWhenCouldNotFindOnLanguageBundle() {
         MessageProducer producer = new MessageProducer(new FileNameCreator("messages"));
-        String message = producer.getMessage("invalid.key", new DefaultLanguage("some_code"));
+        String message = producer.getMessage("invalid.key", new DefaultLanguage("country_code"));
         Assert.assertEquals("??? invalid.key ???", message);
+    }
+
+    @Test
+    public void testThatRespectsCountryCode() {
+        MessageProducer producer = new MessageProducer(new FileNameCreator("messages"));
+        String message = producer.getMessage("test", new DefaultLanguage("pt_BR"));
+        Assert.assertEquals("isto é um teste", message);
     }
 }
